44a4bfc92ae10542a01f29c6b6f20ee7cb44e014
jlferrete <joseluisferrete@gmail.com>
2023-07-24 17:08:30 +0200

    Add exercise 2

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
 // Ejercicio 2
 async function writeFile (filePath, data, callback) {
+  try {
+    const dir = path.dirname(filePath);
+    await fs.mkdir(dir, { recursive: true });
+  } catch (error) {
+    console.log('Error creando directorio', error);
+    process.exit(1);
+  }
+
+  try {
+    await fs.writeFile(filePath, data);
+  } catch (error) {
+    console.log(`Error creando fichero en la ruta: ${filePath}`, error);
+    process.exit(1);
+  }
+
+  callback();
 
 }
 
